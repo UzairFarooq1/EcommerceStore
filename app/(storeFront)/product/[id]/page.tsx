dcd1d5d44ec1f7bad45f23a502b5d56adc91e203
UzairFarooqCG <115949991+UzairFarooq1@users.noreply.github.com>
2024-10-24 10:51:14 +0300

    feat: :rocket: final Commit: Not deployed yet due to change of next  version which led to several package conflicts

--- app/(storeFront)/product/[id]/page.tsx
+++ app/(storeFront)/product/[id]/page.tsx
@@ -1,18 +1,11 @@
 import { ShoppingBagButton } from "@/app/components/SubmitButtons";
 import { FeaturedProducts } from "@/app/components/storefront/FeaturedProducts";
-import { ImageSlider } from "@/app/components/storefront/ImageSlider";
 import prisma from "@/app/lib/db";
-import { addItem } from "@/app/action";
 import { StarIcon } from "lucide-react";
 import { notFound } from "next/navigation";
 import { unstable_noStore as noStore } from "next/cache";
-
-type ProductPageProps = {
-  params: {
-    id: string;
-  };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
+import { ImageSlider } from "@/app/components/storefront/ImageSlider";
+import { addItem } from "@/app/action";
 
 async function getData(productId: string) {
   const data = await prisma.product.findUnique({
@@ -29,17 +22,20 @@ async function getData(productId: string) {
   });
 
   if (!data) {
-    notFound();
+    return notFound();
   }
 
   return data;
 }
 
-export default async function ProductPage({ params }: ProductPageProps) {
+export default async function ProductIdRoute({
+  params,
+}: {
+  params: { id: string };
+}) {
   noStore();
   const data = await getData(params.id);
-  const addProductToShoppingCart = addItem.bind(null, data.id);
-
+  const addProducttoShoppingCart = addItem.bind(null, data.id);
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-start lg:gap-x-24 py-6">
@@ -50,16 +46,15 @@ export default async function ProductPage({ params }: ProductPageProps) {
           </h1>
           <p className="text-3xl mt-2 text-gray-900">${data.price}</p>
           <div className="mt-3 flex items-center gap-1">
-            {[...Array(5)].map((_, i) => (
-              <StarIcon
-                key={i}
-                className="h-4 w-4 text-yellow-500 fill-yellow-500"
-              />
-            ))}
+            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
+            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
+            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
+            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
+            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
           </div>
           <p className="text-base text-gray-700 mt-6">{data.description}</p>
 
-          <form action={addProductToShoppingCart}>
+          <form action={addProducttoShoppingCart}>
             <ShoppingBagButton />
           </form>
         </div>
