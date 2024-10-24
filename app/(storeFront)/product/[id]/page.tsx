8b613eef9d0cc7d4255c2cf80132418baa491c06
UzairFarooqCG <115949991+UzairFarooq1@users.noreply.github.com>
2024-10-24 09:33:06 +0300

    fix: :bug: params error

--- app/(storeFront)/product/[id]/page.tsx
+++ app/(storeFront)/product/[id]/page.tsx
@@ -7,9 +7,10 @@ import { unstable_noStore as noStore } from "next/cache";
 import { ImageSlider } from "@/app/components/storefront/ImageSlider";
 import { addItem } from "@/app/action";
 
-// Define the custom type for params
-type Params = {
-  id: string;
+type ProductPageProps = {
+  params: {
+    id: string;
+  };
 };
 
 async function getData(productId: string) {
@@ -27,20 +28,16 @@ async function getData(productId: string) {
   });
 
   if (!data) {
-    return notFound();
+    notFound();
   }
 
   return data;
 }
 
-export default async function ProductIdRoute({
-  params,
-}: {
-  params: Params; // Use the custom type here
-}) {
+export default async function ProductPage({ params }: ProductPageProps) {
   noStore();
   const data = await getData(params.id);
-  const addProducttoShoppingCart = addItem.bind(null, data.id);
+  const addProductToShoppingCart = addItem.bind(null, data.id);
 
   return (
     <>
@@ -52,15 +49,16 @@ export default async function ProductIdRoute({
           </h1>
           <p className="text-3xl mt-2 text-gray-900">${data.price}</p>
           <div className="mt-3 flex items-center gap-1">
-            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
-            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
-            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
-            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
-            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
+            {[...Array(5)].map((_, i) => (
+              <StarIcon
+                key={i}
+                className="h-4 w-4 text-yellow-500 fill-yellow-500"
+              />
+            ))}
           </div>
           <p className="text-base text-gray-700 mt-6">{data.description}</p>
 
-          <form action={addProducttoShoppingCart}>
+          <form action={addProductToShoppingCart}>
             <ShoppingBagButton />
           </form>
         </div>
